1afd4b4df7ff6476166d3f1e4068039a2f273aef
geetika <geetika2598@gmail.com>
2018-06-19 17:52:39 -0700

    some problems related with list,tuple,dic

---
+++ about_python1.py
@@ -0,0 +1,139 @@
+#practice1
+
+#question1 : what is the output of print str if str='hello world!'.
+
+#this will print Hello World
+str='Hello World'
+print(str)
+
+
+#question2 : what is the output of print str[0] if str='hello world'.
+
+#this will print H 
+print (str[0])
+
+
+#question3 : what is the output of print str[2:5] if str='hello world'.
+	
+#this will print llo
+print (str[2:5])
+
+
+#question4 : what is the output of print str[2:] if str='hello world'
+
+#thi will print llo World
+print(str[2:])
+
+
+#question5: what is the output of print str*2 if str='hello world'.
+
+#this will print Hello WorldHello World
+print(str*2)
+
+
+#question6: what is the output of print str+"TEST" if str='hello world'.
+
+#this will print Hello WorldTEST
+print(str+"TEST")
+
+
+
+#question7: what is the output of print list if list= ['abcd',786,2.23,'john',70.2]
+
+#OUTput:['abcd', 786, 2.23, 'john', 70.2]
+list=['abcd',786,2.23,'john',70.2]
+print(list)
+
+
+#question8: what is the output of print list[0] if list= ['abcd',786,2.23,'john',70.2]
+
+# output : abcd
+print(list[0])
+
+
+#question9: what is the output of print list[1:3] if list= ['abcd',786,2.23,'john',70.2]	
+
+# output : [786,2.23]
+print(list[1:3])
+
+
+#question10: what is the output of print list[2:] if list= ['abcd',786,2.23,'john',70.2]
+
+#ouput : [2.23,'john',70.2]
+print(list[2:])
+
+
+#question11: what is the output of print tuple if tuple=('abcd',786,2.23,'john',70.2)
+
+# output : ('abcd',786,2.23,'john',70.2)
+tuple=('abcd',786,2.23,'john',70.2)
+print(tuple)
+
+
+#question12: what is the output of print tuple[0] if tuple=('abcd',786,2.23,'john',70.2)
+
+# output :  abcd
+print(tuple[0])
+
+
+#question22: what is the output of print tuple[1:3] if tuple=('abcd',786,2.23,'john',70.2)
+
+# output : (786,2.23)
+print(tuple[1:3])
+
+
+#question23: what is the output of print tuple[2:] if tuple=('abcd',786,2.23,'john',70.2)
+
+# output : (2.23,'john',70.2)
+print(tuple[2:])
+
+
+#question24: how you will create a dictionary in python.
+
+output : {'name': 'Geetika', 'age': 19}
+dic={'name':'Geetika','age':19}
+print(dic)
+
+
+#question25: how you will you convert a string to a float in python.
+
+# output : converting a string in to float:- 21122112.0
+
+s='21122112'
+b=float(s)
+print("converting a string in to float:- ",b)
+
+
+#question26: how will you convert a string to a int in python.
+
+
+#method 1
+
+#ouput : converting a string in to integer:- 18
+
+s="10010"
+b=int(s,2)
+print("converting a string in to integer:- ",b)
+
+#method 2
+
+#output : converting a character into integer:-  103
+
+s="g"
+c=ord(s)
+print("converting a character into integer:- ",c)
+
+
+
+#question39: How will you get the length of the string?
+
+t=("geetika")
+print(len(t))
+
+
+#question40: How will you convert a string to all lowercase
+
+t=("GEETIKA")
+print(t.lower())
+
+  
